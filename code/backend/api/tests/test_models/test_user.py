c9cdf569ea90d85d501d8ae41ec610222c9a4ac8
takusomikke <takusomikke@gmail.com>
2022-07-14 14:46:56 +0900

    fix: Change delimiter

--- code/backend/api/tests/test_models/test_user.py
+++ code/backend/api/tests/test_models/test_user.py
@@ -4,10 +4,10 @@ from api.tests.factories import UserFactory
 
 class WorkflowTests(TestCase):
     def setUp(self):
-        self.user = UserFactory(username="next-type")
+        self.user = UserFactory(username="next_type")
 
     def test_username_validName_correctValue(self):
         self.assertEqual(
             self.user.username,
-            "next-type"
+            "next_type"
         )
