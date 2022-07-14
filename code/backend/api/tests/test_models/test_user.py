256d47568f5573f0de5fb0a248d10037c8e051b3
takusomikke <takusomikke@gmail.com>
2022-07-14 16:55:45 +0900

    Modify test

--- code/backend/api/tests/test_models/test_user.py
+++ code/backend/api/tests/test_models/test_user.py
@@ -4,10 +4,10 @@ from api.tests.factories import UserFactory
 
 class WorkflowTests(TestCase):
     def setUp(self):
-        self.user = UserFactory(username="next_type")
+        self.user = UserFactory(username="hoge")
 
     def test_username_validName_correctValue(self):
         self.assertEqual(
             self.user.username,
-            "next_type"
+            "hoge"
         )
